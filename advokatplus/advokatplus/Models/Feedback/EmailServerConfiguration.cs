0d982f9bd16903feac83924bd3d66f5342126495
Alexander Rudich <rudichalex@yandex.ru>
2020-02-18 00:18:54 +0300

    configure MailKit services

---
+++ advokatplus/advokatplus/Models/Feedback/EmailServerConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace advokatplus.Models.Feedback
+{
+    public class EmailServerConfiguration
+    {
+		public EmailServerConfiguration(int _smtpPort = 465)
+		{
+			SmtpPort = _smtpPort;
+		}
+		public string SmtpServer { get; set; }
+		public int SmtpPort { get; }
+		public string SmtpUsername { get; set; }
+		public string SmtpPassword { get; set; }
+	}
+}
